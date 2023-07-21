f685c9fcd6e2eaf4ba8f020b4081dfd2cfd8f699
jeevashhkjs <jeevasdckap@gmail.com>
2023-07-21 15:54:32 +0530

    finished

---
+++ Home-page/js/scripts.js
@@ -0,0 +1,37 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.6 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+// 
+// Scripts
+// 
+// search function
+let searchEle = document.querySelector(".searchInput")
+let searchIcon = document.querySelector(".searchIcon")
+
+searchEle.addEventListener("keyup",()=>{
+    if(searchEle.value){
+        searchIcon.style.display = "none"
+    }else{
+        searchIcon.style.display = "block"
+    }
+})
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
